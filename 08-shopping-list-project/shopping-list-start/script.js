1dbb53d9f09429c43eaa70984b5f2f8ede47c511
Michael LeRoux <946718+mpleroux@users.noreply.github.com>
2025-11-03 06:55:42 -0500

    Initial commit

---
+++ 08-shopping-list-project/shopping-list-start/script.js
@@ -0,0 +1,246 @@
+const itemForm = document.getElementById('item-form');
+const itemInput = document.getElementById('item-input');
+const itemList = document.getElementById('item-list');
+const clearBtn = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
+const formBtn = itemForm.querySelector('button');
+let isEditMode = false;
+
+function displayItems() {
+  const itemsFromStorage = getItemsFromStorage();
+
+  itemsFromStorage.forEach((item) => addItemToDOM(item));
+
+  checkUI();
+}
+
+function onAddItemSubmit(e) {
+  e.preventDefault();
+
+  const newItem = itemInput.value;
+
+  // Validate Input
+  if (newItem === '') {
+    alert('Please add an item');
+    return;
+  }
+
+  // Check for edit mode
+  if (isEditMode) {
+    const itemToEdit = itemList.querySelector('.edit-mode');
+
+    removeItemFromStorage(itemToEdit.textContent);
+    itemToEdit.classList.remove('edit-mode');
+    itemToEdit.remove();
+    isEditMode = false;
+  } else {
+    if (checkIfItemExists(newItem)) {
+      alert('That item already exists.');
+      return;
+    }
+  }
+
+  // Create item DOM element
+  addItemToDOM(newItem);
+
+  // Add item to local storage
+  addItemToStorage(newItem);
+
+  checkUI();
+
+  itemInput.value = '';
+}
+
+function addItemToDOM(item) {
+  // Create list item
+  const li = document.createElement('li');
+  li.appendChild(document.createTextNode(item));
+
+  const button = createButton('remove-item btn-link text-red');
+  li.appendChild(button);
+
+  // Add li to the DOM
+  itemList.appendChild(li);
+}
+
+function createButton(classes) {
+  const button = document.createElement('button');
+  button.className = classes;
+
+  const icon = createIcon('fa-solid fa-xmark');
+  button.appendChild(icon);
+
+  return button;
+}
+
+function createIcon(classes) {
+  const icon = document.createElement('i');
+  icon.className = classes;
+
+  return icon;
+}
+
+function addItemToStorage(item) {
+  const itemsFromStorage = getItemsFromStorage();
+
+  // Add new item to array
+  itemsFromStorage.push(item);
+
+  // Convert to JSON string and set to local storage
+  localStorage.setItem('items', JSON.stringify(itemsFromStorage));
+}
+
+function getItemsFromStorage() {
+  let itemsFromStorage;
+
+  // Check if there are items already in localStorage
+  if (localStorage.getItem('items') === null) {
+    itemsFromStorage = [];
+  } else {
+    itemsFromStorage = JSON.parse(localStorage.getItem('items'));
+  }
+
+  return itemsFromStorage;
+}
+
+function onClickItem(e) {
+  if (e.target.parentElement.classList.contains('remove-item')) {
+    removeItem(e.target.parentElement.parentElement);
+  } else {
+    setItemToEdit(e.target);
+  }
+}
+
+function checkIfItemExists(item) {
+  const itemsFromStorage = getItemsFromStorage();
+
+  return itemsFromStorage.includes(item);
+}
+
+function setItemToEdit(item) {
+  isEditMode = true;
+
+  itemList.querySelectorAll('li').forEach((i) => {
+    i.classList.remove('edit-mode');
+  });
+
+  item.classList.add('edit-mode');
+  formBtn.innerHTML = '<i class="fa-solid fa-pen"></i> Update Item';
+  formBtn.style.backgroundColor = '#228b22';
+  itemInput.value = item.textContent;
+}
+
+function removeItem(item) {
+  if (confirm('Are you sure?')) {
+    // Remove item from DOM
+    item.remove();
+
+    // Remove item from storage
+    removeItemFromStorage(item.textContent);
+
+    checkUI();
+  }
+}
+
+function removeItemFromStorage(item) {
+  let itemsFromStorage = getItemsFromStorage();
+
+  // Filter out item to be removed
+  itemsFromStorage = itemsFromStorage.filter((i) => i !== item);
+
+  // Re-set to local storage
+  localStorage.setItem('items', JSON.stringify(itemsFromStorage));
+}
+
+function clearItems(e) {
+  // itemList.innerHTML = '';
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
+  }
+
+  // Clear from local storage
+  localStorage.removeItem('items');
+
+  checkUI();
+}
+
+function checkUI() {
+  itemInput.value = '';
+
+  const items = itemList.querySelectorAll('li');
+
+  if (items.length === 0) {
+    itemFilter.style.display = 'none';
+    clearBtn.style.display = 'none';
+  } else {
+    itemFilter.style.display = 'block';
+    clearBtn.style.display = 'block';
+  }
+
+  formBtn.innerHTML = '<i class="fa-solid fa-plus"></i> Add Item';
+  formBtn.style.backgroundColor = '#333';
+
+  isEditMode = false;
+}
+
+// My filter function
+const onInput = (e) => {
+  const items = itemList.querySelectorAll('li');
+
+  items.forEach((item) => {
+    /*
+    - get value of text node (which should be the first child)
+    - get value of itemFilter
+    - why is it one keypress behind? (use input, not keypress)
+    - compare substring of each item (using current itemFilter length) to itemFilter
+    - if not equal, set display to none
+    - otherwise set display to '' (styling of close button breaks if you use block or inline)
+    */
+
+    if (
+      item.firstChild.nodeValue.substring(0, itemFilter.value.length) !==
+      itemFilter.value
+    ) {
+      item.style.display = 'none';
+    } else {
+      item.style.display = '';
+    }
+  });
+};
+
+/*
+- use conventional function for event listener
+- make both parts of comparison lowercase
+- use textContent to get text node
+- match any occurrance of input string, not just at beginning of item
+- default display property was set to 'flex' in DevTools
+*/
+function filterItems(e) {
+  const items = itemList.querySelectorAll('li');
+  const text = e.target.value.toLowerCase();
+
+  items.forEach((item) => {
+    itemName = item.firstChild.textContent.toLowerCase().trim();
+
+    // find any occurrance of string that was entered
+    if (itemName.indexOf(text) != -1) {
+      item.style.display = 'flex';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+}
+
+// Initialize app
+function init() {
+  // Event Listeners
+  itemForm.addEventListener('submit', onAddItemSubmit);
+  itemList.addEventListener('click', onClickItem);
+  clearBtn.addEventListener('click', clearItems);
+  itemFilter.addEventListener('input', filterItems);
+  document.addEventListener('DOMContentLoaded', displayItems);
+
+  checkUI();
+}
+
+init();
---
+++ README.md
@@ -0,0 +1,23 @@
+# README
+
+## Description
+
+These are the local files I created while following the full 38 hour Traversy Media course "Modern JavaScript From The Beginning 2.0." The first part is available on YouTube at [React Crash Course](https://www.youtube.com/watch?v=BI1o2H9z9fo).
+
+- [Project repository](https://github.com/bradtraversy/javascript-sandbox)
+
+I cloned the project repo to start each lesson with scaffolding like HTML files and stylesheets. For there I followed the tutorials and created everything from scratch. My version of the repo contains a handful of directories for self-contained projects like the Shopping List.
+
+### Technologies used
+
+- JavaScript
+
+## To install and run from repo
+
+Everything is run locally using the Live Server VSCode extension.
+
+### Notes about tutorial
+
+## Screenshots
+
+![Screenshot of Shopping List project](./08-shopping-list-project/shopping-list-start/images/screenshot.png "Screenshot of Shopping List project")
